b436d4e7c1a79dfa4ecbae149a8da7fc1356d4a8
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-09 09:44:14 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCitasComponent,
+    CrearCitaComponent
   ],
   imports: [
     BrowserModule
