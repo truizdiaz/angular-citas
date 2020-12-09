b9517aa7df43cd46a8e4b1c2c694fabd54056aa6
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-09 11:52:09 -0300

    @Input ListCitas

--- src/app/components/list-citas/list-citas.component.ts
+++ src/app/components/list-citas/list-citas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-citas',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-citas.component.css']
 })
 export class ListCitasComponent implements OnInit {
+  @Input() listadoCitas: any;
 
   constructor() { }
 
